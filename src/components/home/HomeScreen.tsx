690314ab95cdfa4d58a293669138a2011b1fb7e7
tomxdiaz <tomydiaz739@gmail.com>
2026-05-14 08:28:06 -0300

    ngrok

--- src/components/home/HomeScreen.tsx
+++ src/components/home/HomeScreen.tsx
@@ -16,6 +16,7 @@ export default function HomeScreen() {
         setRestaurants(data);
       } catch (error) {
         console.error('Error fetching restaurants:', error);
+        setRestaurants([]);
       }
     };
 
