b4efeaab7cca5c497dc0a5c79b2f37ec8110b7fd
tomxdiaz <tomydiaz739@gmail.com>
2026-05-26 12:40:22 -0300

    fix

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,6 +1,5 @@
 import { apiClient } from '../lib/apiClient';
 import { Order } from '../types/types';
-import { restaurantService } from './restaurant.service';
 
 export const orderService = {
   getMyOrders: async () => {
