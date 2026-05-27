f82c0e8991e512e19936999f985212dc8aaddd95
tomxdiaz <tomydiaz739@gmail.com>
2026-05-27 11:08:34 -0300

    cosas

--- src/components/restaurants/Detail/MyRestaurantDetailScreen.tsx
+++ src/components/restaurants/Detail/MyRestaurantDetailScreen.tsx
@@ -14,13 +14,10 @@ const MyRestaurantDetailScreen = ({ id }: { id: string }) => {
       try {
         const myRestaurant = await restaurantService.getRestaurantById(id);
         setMyRestaurant(myRestaurant);
-        console.log('Restaurante obtenido name:', myRestaurant.name);
         const myRestaurantStaffInfo = await restaurantStaffService.getMyRestaurantStaffInfo(id);
-        console.log('Información del staff obtenida rol:', myRestaurantStaffInfo.role);
         setMyRestaurantStaffInfo(myRestaurantStaffInfo);
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching restaurant:', error);
         setMyRestaurant(null);
         setMyRestaurantStaffInfo(null);
         setLoading(false);
@@ -30,13 +27,6 @@ const MyRestaurantDetailScreen = ({ id }: { id: string }) => {
     fetchData();
   }, []);
 
-  return (
-    <View>
-      <Text>myRestaurant name: {myRestaurant?.name}</Text>
-      <Text>myRestaurant rol: {myRestaurantStaffInfo?.role}</Text>;
-    </View>
-  );
-
   if (!myRestaurant) {
     return <Text>El restaurante no existe</Text>;
   }
@@ -45,13 +35,13 @@ const MyRestaurantDetailScreen = ({ id }: { id: string }) => {
     return <Text>No formas parte de este restaurante</Text>;
   }
 
-  //   return (
-  //     <View>
-  //       <Text>{`Restaurant ID: ${myRestaurant.id}`}</Text>
-  //       <Text>{myRestaurant.name}</Text>
-  //       {/* <Text>{`Mi rol en este restaurante: ${myRestaurantStaffInfo.role}`}</Text> */}
-  //     </View>
-  //   );
+  return (
+    <View>
+      <Text>{`Restaurant ID: ${myRestaurant.id}`}</Text>
+      <Text>{myRestaurant.name}</Text>
+      <Text>{`Mi rol en este restaurante: ${myRestaurantStaffInfo.role}`}</Text>
+    </View>
+  );
 };
 
 export default MyRestaurantDetailScreen;
