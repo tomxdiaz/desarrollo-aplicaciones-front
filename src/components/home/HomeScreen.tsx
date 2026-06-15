06e70f9a0e96ccddef64a80b8a3deeb883b060c1
tomxdiaz <tomydiaz739@gmail.com>
2026-06-15 16:23:39 -0300

    single sonar fix in order to test

--- src/components/home/HomeScreen.tsx
+++ src/components/home/HomeScreen.tsx
@@ -1,25 +1,8 @@
 import { Redirect } from 'expo-router';
-import { Button, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
   return <Redirect href='/restaurants' />;
-
-  // return (
-  //   <View style={styles.container}>
-  //     {appUser && <Text>Bienvenido, {appUser.email}!</Text>}
-  //     {appUser && <Text>Tu ID es: {appUser.id}!</Text>}
-  //     {appUser && <Text>Tenes el rol: {appUser.global_role}!</Text>}
-
-  //     {appUser && <Button title='Cerrar sesión' onPress={signOut} />}
-
-  //     <Button title='Ir a Login' onPress={() => router.navigate('/signin')} />
-  //     <Button title='Ir a Register' onPress={() => router.navigate('/register')} />
-
-  //     <Text>Provecho!</Text>
-
-  //     <StatusBar style='auto' />
-  //   </View>
-  // );
 }
 
 const styles = StyleSheet.create({
